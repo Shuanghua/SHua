06cfbdb4e331b62057695580f5d7d6b48d376341
ShuangHua <moshuanghua17@gmail.com>
2016-03-11 18:34:55 +0800

    4.4通知栏适配

---
+++ app/src/main/java/com/shuanghua/shua/view/activity/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.shuanghua.shua.view.activity;
+
+import android.os.Bundle;
+
+import com.jude.beam.bijection.BeamAppCompatActivity;
+
+/**
+ * Created by ShuangHua on 2016/3/11.
+ */
+public class BaseActivity extends BeamAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/shuanghua/shua/view/activity/MainActivity.java
+++ app/src/main/java/com/shuanghua/shua/view/activity/MainActivity.java
@@ -11,51 +11,61 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.jude.beam.bijection.BeamAppCompatActivity;
 import com.shuanghua.shua.R;
+import com.shuanghua.shua.view.widget.StatusBarCompat;
 
-public class MainActivity extends BeamAppCompatActivity
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
-    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @InjectView(R.id.collapsingToolbarLayout)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    @InjectView(R.id.fab)
+    FloatingActionButton fab;
+    @InjectView(R.id.drawer_layout)
+    DrawerLayout drawer;
+    @InjectView(R.id.nav_view)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
-        mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        setToolbar();
+        setToggle();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
-        mCollapsingToolbarLayout.setTitle(getResources().getString(R.string.toolbar_titel));
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    private void setToolbar() {
+        //mCollapsingToolbarLayout.setTitle(getResources().getString(R.string.toolbar_titel));
         setSupportActionBar(toolbar);
+        mCollapsingToolbarLayout.setTitleEnabled(false);
+        StatusBarCompat.compat(this);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    private void setToggle() {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+    }
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    @OnClick(R.id.fab)
+    public void fabOnClick() {
+        Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -65,19 +75,14 @@ public class MainActivity extends BeamAppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
@@ -88,11 +93,9 @@ public class MainActivity extends BeamAppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -104,8 +107,6 @@ public class MainActivity extends BeamAppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
